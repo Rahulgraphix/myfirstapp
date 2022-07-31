d44099d432fbaeb3df3d581ffc7f7705e3300900
unknown <rahulrk.rr52@gmail.com>
2022-07-31 19:28:39 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/Screen/home.dart';
 import 'package:myapp/Screen/loin_page.dart';
+import 'package:myapp/util/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,8 +22,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => Homepage(),
-        "/Login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
